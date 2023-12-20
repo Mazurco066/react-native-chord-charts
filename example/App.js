f7fb040d5b5d8f122ba7270c49c1e78bb04e00e2
Gabriel Mazurco (RaktaTech) <mazurco066@gmail.com>
2023-12-20 00:10:28 -0300

    fix(npm): package peer dependencies compatibility

--- example/App.js
+++ example/App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ChordChart from 'react-native-chord-charts';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <ChordChart
+        key="G"
+        color="#F84D28"
+        showTuning
+      />
       <StatusBar style="auto" />
     </View>
   );
--- src/utils/getChordPositions.ts
+++ src/utils/getChordPositions.ts
@@ -1,9 +1,8 @@
 import chords from '../vendors/chords.json'
 
 export const getChordPositions = (key: string) => {
-  const sanitizedKey: string = key.replace(/\s/g, '')
-  if (chords.hasOwnProperty(sanitizedKey)) {
-    const chordObj = chords[sanitizedKey].find(() => true)
+  if (chords.hasOwnProperty(key)) {
+    const chordObj = chords[key].find(() => true)
     return (chordObj !== null)
       ? chordObj.positions
       : ['x', 'x', 'x', 'x', 'x', 'x']
