63dd26aaeaf05aa2335b5f5cc18f9c11495d1e3f
Gabriel Mazurco <mazurco066@gmail.com>
2023-12-20 10:59:55 -0300

    build(1.0.5): build 1.0.5 for lightweight chords data

--- example/App.js
+++ example/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <ChordChart
-        chordKey='G'
+        chordKey='G5'
         color="#F84D28"
         showTuning
       />
--- src/utils/getChordPositions.ts
+++ src/utils/getChordPositions.ts
@@ -2,11 +2,7 @@ import chords from '../vendors/chords.json'
 
 export const getChordPositions = (key: string) => {
   if (chords.hasOwnProperty(key)) {
-    const chordObj = chords[key].find(() => true)
-    return (chordObj !== null)
-      ? chordObj.positions
-      : ['x', 'x', 'x', 'x', 'x', 'x']
-  } else {
-    return ['x', 'x', 'x', 'x', 'x', 'x']
+    return chords[key]
   }
+  return ['x', 'x', 'x', 'x', 'x', 'x']
 }
